8f84f4f69218d6f86fb518aae385a7430c1607bd
Tami <tklankford@aol.com>
2018-01-18 14:36:14 -0800

    Added Detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Comic Book Controller";
+        }
     }
 }
